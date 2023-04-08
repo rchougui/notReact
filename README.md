fc3e928a5483c0c7f4ef2cf40c7b9052e605d546
Riadh Chougui <chougui.riadh@gmail.com>
2023-04-08 03:15:17 +0100

    add fetch calls using useEffect

---
+++ README.md
@@ -0,0 +1,11 @@
+## NotReact
+
+simplified implementation of React hooks in typescript based on the great talk by Shawn "Swyx" Wang [Link to youtube video](https://www.youtube.com/watch?v=KJP1E-Y-xyo)
+
+## Checklist
+
+- [x] useState hook
+- [x] useEffect
+- [x] JSX support using typescript
+- [ ] Custom hooks
+- [ ] Development server
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,41 @@ import { React } from "./lib/notReact";
 
 const Component = () => {
   const [count, setCount] = React.useState(1);
+  const [list, setList] = React.useState([]);
   const [text, setText] = React.useState<string>("This is not React !");
   React.useEffect(() => {
-    console.log("useEffect in effect !");
-  }, [text]);
+    fetch("https://dog.ceo/api/breeds/image/random/" + count)
+      .then((x) => x.json())
+      .then((x) => setList(x.message));
+    console.log(list);
+  }, [count]);
+
   return (
-    <>
-      test
-      {count}
-      <button onClick={() => setCount(count + 1)}></button>
-    </>
+    <main>
+      <div>
+        Text: {text}
+        <button onClick={() => setText("and yet it reacts !")}>
+          Change text
+        </button>
+      </div>
+      <div>
+        Count: {count}
+        <button onClick={() => setCount(count + 1)}>
+          Increase count, trigger useEffect
+        </button>
+        <div style="display:flex; width:100vw; flex-wrap:wrap">
+          {list.map((item) => (
+            <img
+              style="max-width:20%; max-height:200px;
+               padding:1em; object-fit: cover;"
+              src={item}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 };
 
 const rootElement = document.getElementById("root");
 React.render(<Component />, rootElement);
-
-console.log("test");
